3b52a455725449c746a687e6f97408d8b97b98c2
AndrewB4y <andresbaymon@gmail.com>
2021-11-29 17:39:12 -0500

    [Documentation] Adding documentation to file 2-size_me_please

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    """
+    matrix_shape(matrix) - calculates the shape of a matrix
+
+    @matrix: nested object (often nested list) to get its' shape
+
+    Returns: the dimensions of @matrix in a list.
+    """
     result = []
     a = matrix
     m_class = type(matrix)
@@ -8,4 +15,3 @@ def matrix_shape(matrix):
         result.append(len(a))
         a = a[0]
     return result
-    
