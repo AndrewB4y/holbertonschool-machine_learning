2977dad601acfef40072b74edb8da63d7b8f0791
AndrewB4y <andresbaymon@gmail.com>
2021-11-29 21:34:43 -0500

    [new file] Adding file 4: array adition element-wise

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,19 @@
+#!/etc/usr/env python3
+
+
+def add_arrays(arr1, arr2):
+    """
+    add_arrays(arr1, arr2) - adds two arrays element-wise
+
+    @arr1: list of ints/floats
+    @arr2: list of ints/floats
+
+    Returns: A new list as result of adding element-wise @arr1 with @arr2
+    """
+    lenght = len(arr1)
+    if lenght != len(arr2):
+        return None
+    result = []
+    for i in range(lenght):
+        result.append(arr1[i] + arr2[i])
+    return result
