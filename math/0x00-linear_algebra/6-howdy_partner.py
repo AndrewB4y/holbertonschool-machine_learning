477eca88773dd5aca20a6471e6e23be874969dae
AndrewB4y <andresbaymon@gmail.com>
2021-11-29 22:18:34 -0500

    [new file] Adding file 6: concatenation of two arrays.

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,20 @@
+#!/etc/usr/env python3
+
+def cat_arrays(arr1, arr2):
+    """
+    cat_arrays(arr1, arr2) - concatenates two arrays.
+
+    @arr1: list of ints/floats
+    @arr2: list of ints/floats
+
+    Returns: A new list as result of the concatenation.
+    """
+
+    if arr1 is None:
+        return arr2
+    elif arr2 is None:
+        return arr1
+    else:
+        res = [x for x in arr1]
+        res.extend(arr2)
+        return res
