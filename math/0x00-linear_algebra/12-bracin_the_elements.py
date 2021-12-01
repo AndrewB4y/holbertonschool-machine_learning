4e209509cb335e0807a3efa0fefe9f7ef61bc31f
AndrewB4y <andresbaymon@gmail.com>
2021-12-01 08:26:28 -0500

    [code] removing parenthesis at return statement

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -17,4 +17,4 @@ def np_elementwise(mat1, mat2):
              and quotient, respectively.
     """
 
-    return (mat1+mat2, mat1-mat2, mat1*mat2, mat1/mat2)
+    return mat1+mat2, mat1-mat2, mat1*mat2, mat1/mat2
