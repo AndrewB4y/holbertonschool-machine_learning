d08d40ba6b4d6c4bee435b856d1b32731155b7ab
AndrewB4y <andresbaymon@gmail.com>
2021-12-01 04:24:29 -0500

    [new file] Adding file 10: calculate the shape of a np array

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+ill_use_my_scale module
+"""
+
+
+def np_shape(matrix):
+    """
+    np_shape(matrix) - calculates the shape of a numpy.ndarray
+
+    @matrix: a numpy.ndarray
+
+    Returns: a tuple of integers with the shape of @matrix
+    """
+
+    return matrix.shape
