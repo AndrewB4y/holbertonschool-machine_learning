dff57a11defcde994852532233514ded763e0569
AndrewB4y <andresbaymon@gmail.com>
2021-12-01 04:39:48 -0500

    [new file] Adding file 11: transposing a numpy array

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+the_western_exchange module
+"""
+
+
+def np_transpose(matrix):
+    """
+    np_transpose(matrix) - transposes matrix.
+
+    @matrix: a numpy.ndarray
+
+    Returns: a new numpy.ndarray which is the transpose of @matrix
+    """
+
+    return np.transpose(matrix)
