181f440db1281432c05903492b46fa8c4c1fc3c0
AndrewB4y <andresbaymon@gmail.com>
2021-12-01 22:59:02 -0500

    [new file] Adding file10: Slicing matrix along specific axes.

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+"""
+slice_like_a_ninja module
+"""
+
+
+def np_slice(matrix, axes={}):
+    """
+    np_slice(matrix, axes={}) -  slices a matrix along specific axes.
+
+    @matrix: a numpy.ndarray
+    @axes: a dictionary where the key is an axis to slice along and
+           the value is a tuple representing the slice to make along
+           that axis.
+           @axes must always represent a valid slice
+
+    Returns: a new numpy.ndarray which is a slice of @matrix.
+
+    Example:
+    $ mat2 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]],
+                 [[21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]])
+    $ print(np_slice(mat2, axes={0: (2,), 2: (None, None, -2)}))
+                                 mat2[:2,:,::-2]
+    $ print(mat2)
+        [[[ 5  3  1]
+          [10  8  6]]
+
+         [[15 13 11]
+          [20 18 16]]]
+    """
+
+    ninja = [":", ]*(max(axes.keys()) + 1)
+    for axis, params in axes.items():
+        if len(params) == 1:
+            ninja[axis] = ":{}".format(params[0])
+        else:
+            temp = ""
+            for idx, i in enumerate(params):
+                if idx != 0:
+                    temp = temp + ":"
+                if i is None:
+                    continue
+                temp = temp + str(i)
+            ninja[axis] = temp
+    my_slice = matrix.copy()
+    my_ninja = "my_slice[{}]".format(', '.join(ninja))
+    my_slice = eval(my_ninja)
+
+    return my_slice
