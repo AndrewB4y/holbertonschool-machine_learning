29e2f6b8f3f0d1fda88160d1f9d78fa38fd27360
AndrewB4y <andresbaymon@gmail.com>
2021-12-03 01:22:11 -0500

    [code] Completing the functional code 102

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -36,40 +36,38 @@ def cat_matrices(mat1, mat2, axis=0):
         b = b[0]
         b_shape.append(l_b)
 
-    a_s = [x for x in a_shape]
-    del a_s[axis]
-    b_s = [x for x in b_shape]
-    del b_s[axis]
+    a_s = a_shape.copy()
+    a_s[axis] = 0
+    b_s = b_shape.copy()
+    b_s[axis] = 0
     if a_s != b_s:
         return None
 
-    ndim = len(a_shape)
-    current = [0]*ndim
-    str_ndim = '0,'
-    for d in a_shape[:0:-1]:
-        str_ndim = '[' + str_ndim*d + '],'
-    str_ndim = '[' + str_ndim*a_shape[0] + ']'
-    # str_ndim = '[0]*{}'.format(a_shape[-1])
-    # for dim in a_shape[-2::-1]:
-    #     str_ndim = '[' + str_ndim + ']*{}'.format(dim)
-    result = eval(str_ndim)
-    # while current[0] < a_shape[0]:
-    #     #  Dinamically accesing indices
-    #     indices = "[" + "][".join([str(x) for x in current])+"]"
-    #     n_mat1 = eval("mat1"+indices)
-    #     n_mat2 = eval("mat2"+indices)
-    #     exec("result"+indices+" = {}".format(n_mat1+n_mat2))
-    #     #  Updating indices in list current
-    #     dim = ndim
-    #     while dim:
-    #         dim = dim - 1
-    #         if current[dim] == a_shape[dim] - 1:
-    #             if dim == 0:
-    #                 current[dim] += 1
-    #                 break
-    #             current[dim] = 0
-    #             continue
-    #         current[dim] += 1
-    #         dim = 0
+    c_mat1 = eval(str(mat1))
+    c_mat2 = eval(str(mat2))
+
+    result = c_mat1
+    if axis == 0:
+        result.extend(c_mat2)
+        return result
+
+    current = [0,]*len(a_s[:axis])
+    while current[0] < a_shape[0]:
+        #  Dinamically accesing indices
+        indices = "[" + "][".join([str(x) for x in current])+"]"
+        exec("result"+indices+".extend(c_mat2"+indices+")")
+        #  Updating indices in list current
+        dim = axis
+        while dim:
+            dim = dim - 1
+            if current[dim] == a_shape[dim] - 1:
+                if dim == 0:
+                    current[dim] += 1
+                    # reaching full dimensions: breaking out of loop
+                    break
+                current[dim] = 0
+                continue
+            current[dim] += 1
+            dim = 0
 
     return result
