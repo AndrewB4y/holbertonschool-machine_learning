aa1e52d8d851d8604b52ab50bbc79a947c54a29d
AndrewB4y <andresbaymon@gmail.com>
2021-12-06 22:41:20 -0500

    [style] Complying with pycodestyle

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-bins=np.arange(0, 100, 10)
+bins = np.arange(0, 100, 10)
 n, bins, patches = plt.hist(student_grades, bins=bins, edgecolor='k')
 
 
 plt.xlabel('Grades')
 plt.ylabel('Number of Students')
 plt.title('Project A')
-#plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+# plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
 plt.axis([0, 100, 0, 30])
 plt.show()
