f44f8c55cb92668b93abb567a16583a4005a2de8
AndrewB4y <andresbaymon@gmail.com>
2021-12-06 20:59:06 -0500

    [code] Fixing axis labels

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -6,14 +6,12 @@ np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
 bins=np.arange(0, 100, 10)
-
 n, bins, patches = plt.hist(student_grades, bins=bins, edgecolor='k')
 
 
-plt.xlabel('Smarts')
-plt.ylabel('Probability')
-plt.title('Histogram of IQ')
-plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+#plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
 plt.axis([0, 100, 0, 30])
-plt.grid(True)
 plt.show()
