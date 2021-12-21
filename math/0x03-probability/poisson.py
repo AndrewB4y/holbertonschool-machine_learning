c4aa3c70ac5017645967a85766392ec226cf8646
AndrewB4y <andresbaymon@gmail.com>
2021-12-21 03:06:17 -0500

    [new file] File 0: poisson class calc lamdba

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+"""
+poisson module
+"""
+
+
+class Poisson:
+    """
+    Poisson class
+    """
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Poisson class initializer
+        @data: is a list of the data to be used to estimate the distribution.
+        @lambtha: is the expected number of occurences in a given time frame.
+        """
+
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+            return
+
+        if type(data) is not list:
+            print(type(data))
+            raise TypeError("data must be a list")
+        elif len(data) <= 1:
+            raise ValueError("data must contain multiple values")
+
+        self.lambtha = sum(data)/len(data)
