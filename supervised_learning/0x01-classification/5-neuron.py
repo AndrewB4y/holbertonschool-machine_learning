0e0b4b6c706a013039209a6d1b76479ad7787123
AndrewB4y <andresbaymon@gmail.com>
2021-12-30 16:37:14 -0500

    [code] Fixing dimentions in bias parameter

--- supervised_learning/0x01-classification/5-neuron.py
+++ supervised_learning/0x01-classification/5-neuron.py
@@ -137,8 +137,8 @@ class Neuron:
         m = Y.shape[1]
 
         dZ = A - Y
-        dW = np.dot(dZ, X.T) / m
-        db = np.sum(dZ, axis=1, keepdims=True) / m
+        dW = np.matmul(dZ, X.T) / m
+        db = np.sum(dZ) / m
 
         self.__W = self.__W - alpha * dW
         self.__b = self.__b - alpha * db
