[code] Fixing db1 calculation

supervised_learning/0x01-classification/13-neural_network.py
@@ -170,13 +170,12 @@ class NeuralNetwork:
         dW2 = np.matmul(dZ2, A1.T) / m
         db2 = np.sum(dZ2, axis=1, keepdims=True) / m
 
-        self.__W2 = self.__W2 - alpha * dW2
-        self.__b2 = self.__b2 - alpha * db2
-
         dA1 = A1 * (1 - A1)
         dZ1 = np.matmul(self.__W2.T, dZ2) * dA1
         dW1 = np.matmul(dZ1, X.T) / m
         db1 = np.sum(dZ1, axis=1, keepdims=True) / m
 
+        self.__W2 = self.__W2 - alpha * dW2
+        self.__b2 = self.__b2 - alpha * db2
         self.__W1 = self.__W1 - alpha * dW1
         self.__b1 = self.__b1 - alpha * db1