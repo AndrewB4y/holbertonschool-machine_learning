35fc20a873150fcb1ccba4227c121c37e9e18f86
AndrewB4y <andresbaymon@gmail.com>
2021-12-31 19:26:19 -0500

    [new file] DeepNN class

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+"""
+DeepNeuralNetwork module - defines a deep neural network performing
+                        binary classification.
+"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    DeepNeuralNetwork - defines a deep neural network performing
+                        binary classification.
+    """
+
+    def __init__(self, nx, layers):
+        """
+        DeepNeuralNetwork(nx) - DeepNeuralNetwork constructor
+
+        @nx: is the number of input features.
+        @layers: a list representing the number of nodes in each layer of
+                 the network.
+                 The first value in layers represents the number of nodes
+                 in the first layer, …
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if type(layers) is not list:
+            raise TypeError("layers must be a list of positive integers")
+        elif not all(nodes > 0 for nodes in layers):
+            raise ValueError("layers must be a list of positive integers")
+
+        # Number of layers in the neural network.
+        self.L = len(layers)
+
+        # Dictionary to hold all intermediary values of the network.
+        self.cache = {}
+
+        # Dictionary to hold all weights and biased of the network.
+        self.weights = {}
+        for i in range(len(layers)):
+            # position 0 of @layers correspond to the layer 1...
+            if i == 0:
+                W = np.random.randn(layers[i], nx)*np.sqrt(2/nx)
+                self.weights['W1'] = W
+                self.weights['b1'] = np.zeros((layers[i], 1))
+                continue
+
+            W = np.random.randn(
+                layers[i], layers[i-1])*np.sqrt(2/layers[i-1])
+            # offset the @i to correspond i=0 with W1.
+            self.weights['W' + str(i + 1)] = W
+            self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
